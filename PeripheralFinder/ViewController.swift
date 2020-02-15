11b798b2a4a2faf02e10b402654ab3a08e54df29
Md. Yousuf Ali <yousuf.csebd@gmail.com>
2020-02-15 14:00:22 +0600

    Discover peripheral devices and connect

--- PeripheralFinder/ViewController.swift
+++ PeripheralFinder/ViewController.swift
@@ -7,14 +7,68 @@
 //
 
 import UIKit
+import CoreBluetooth
 
 class ViewController: UIViewController {
-
+    
+    var centralManager: CBCentralManager!
+    var otherPeripheral: CBPeripheral!
+    
+    let batteryServiceCBUUID = CBUUID(string: "0x180F")
+    let bloodPressureServiceCBUUID = CBUUID(string: "0x1810")
+    let otherServiceCBUUID = CBUUID(string: "0xFEE0")
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+        
+        
+        
     }
+}
 
-
+extension ViewController: CBCentralManagerDelegate {
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        switch central.state {
+        case .unknown:
+            print("central.state is .unknown")
+        case .resetting:
+            print("central.state is .resetting")
+        case .unsupported:
+            print("central.state is .unsupported")
+        case .unauthorized:
+            print("central.state is .unauthorized")
+        case .poweredOff:
+            print("central.state is .poweredOff")
+        case .poweredOn:
+            print("central.state is .poweredOn")
+            
+//            centralManager.scanForPeripherals(withServices: nil)
+            centralManager.scanForPeripherals(withServices: [batteryServiceCBUUID, bloodPressureServiceCBUUID, otherServiceCBUUID])
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        
+        print("didDiscover: \(peripheral)!")
+        
+        otherPeripheral = peripheral
+        centralManager.stopScan()
+        
+        centralManager.connect(otherPeripheral)
+        
+//        print("advertisementData: \(advertisementData)!")
+        
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        print("didConnect \(peripheral)")
+    }
+    
+    
+    
 }
 
