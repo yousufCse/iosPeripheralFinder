5396839d23f7ec670438f1647b96494e3e373962
Md. Yousuf Ali <yousuf.csebd@gmail.com>
2020-02-15 15:44:40 +0600

    Added multiple service CBUUID name

--- PeripheralFinder/ViewController.swift
+++ PeripheralFinder/ViewController.swift
@@ -12,15 +12,21 @@ import CoreBluetooth
 class ViewController: UIViewController {
     
     var centralManager: CBCentralManager!
-    var otherPeripheral: CBPeripheral!
+    var miBrand4Peripheral: CBPeripheral!
     
-    let batteryServiceCBUUID = CBUUID(string: "0x180F")
-    let bloodPressureServiceCBUUID = CBUUID(string: "0x1810")
+    let batteryServiceCBUUID = CBUUID(string: "0x180F") // Battery
+    let bloodPressureServiceCBUUID = CBUUID(string: "0x1810") // Blood Presure
     let miBrand4ServiceCBUUID = CBUUID(string: "0xFEE0") // MI Brand 4
-    
+    let heartRateServiceCBUUID = CBUUID(string: "0x180D") // Heart Rate
+    let currentTimeServiceCBUUID = CBUUID(string: "0x1805") // Curret Time Service
+    let deviceInfoServiceCBUUID = CBUUID(string: "0x180A") // Device Information
+    let userDataServiceCBUUID = CBUUID(string:"0x181C") // User Data
+    let immediateAlertServiceCBUUID = CBUUID(string: "0x1802") // Immediate Alert
+    let alertNotificationServiceCBUUID = CBUUID(string: "0x1811") // Alert Notification Service
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Called viewDidLoad")
         
         centralManager = CBCentralManager(delegate: self, queue: nil)
         
@@ -34,17 +40,17 @@ extension ViewController: CBCentralManagerDelegate {
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
         switch central.state {
         case .unknown:
-            print("central.state is .unknown")
+            print("Bluetooth State is .unknown")
         case .resetting:
-            print("central.state is .resetting")
+            print("Bluetooth State is .resetting")
         case .unsupported:
-            print("central.state is .unsupported")
+            print("Bluetooth State is .unsupported")
         case .unauthorized:
-            print("central.state is .unauthorized")
+            print("Bluetooth Stateis .unauthorized")
         case .poweredOff:
-            print("central.state is .poweredOff")
+            print("Bluetooth State is .poweredOff")
         case .poweredOn:
-            print("central.state is .poweredOn")
+            print("Bluetooth State is .poweredOn")
             
 //            centralManager.scanForPeripherals(withServices: nil)
             centralManager.scanForPeripherals(withServices: [batteryServiceCBUUID, bloodPressureServiceCBUUID, miBrand4ServiceCBUUID])
@@ -54,17 +60,19 @@ extension ViewController: CBCentralManagerDelegate {
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
         print("Discovered: \(peripheral)!")
         
-        otherPeripheral = peripheral
-        otherPeripheral.delegate = self
+        miBrand4Peripheral = peripheral
+        miBrand4Peripheral.delegate = self
         
         centralManager.stopScan()
-        centralManager.connect(otherPeripheral)
+        centralManager.connect(miBrand4Peripheral)
 //        print("advertisementData: \(advertisementData)!")
     }
     
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
         print("Connected \(peripheral)")
-        otherPeripheral.discoverServices(nil)
+//        miBrand4Peripheral.discoverServices(nil)
+        miBrand4Peripheral.discoverServices([heartRateServiceCBUUID, deviceInfoServiceCBUUID ])
+        
     }
 }
 
